3f794a078559546020c11394d1b4378164eb4643
Andrii Yuskov <andrii.yuskov@gmail.com>
2025-04-08 02:31:42 +0200

    Fix for filtering by tariff type

--- Frontend/Verivox/src/app/electricity-page/electricity-page.component.ts
+++ Frontend/Verivox/src/app/electricity-page/electricity-page.component.ts
@@ -1,47 +1,3 @@
-/**import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-@Component({
-  selector: 'app-electricity-page',
-  imports: [CommonModule, FormsModule],
-  templateUrl: './electricity-page.component.html',
-  styleUrl: './electricity-page.component.css'
-})
-
-export class ElectricityPageComponent {
-  kWh: number = 0;
-  results: any[] = [];
-  filter: string = "all";
-  sort: string = "price-asc";
-
-  constructor(private http: HttpClient) { }
-
-  getComparisons() {
-    this.http.get<any[]>(`http://localhost:5050/api/calculate/?kwh=${this.kWh}`)
-      .subscribe(data => {
-        this.results = data;
-      });
-  }
-
-  filteredResults() {
-    return this.results.filter(result => 
-      this.filter === "all" || 
-      (this.filter === "basic" && result.type === 1) || 
-      (this.filter === "package" && result.type === 2)
-    ).sort((a, b) => {
-      if (this.sort === "price-asc") return a.annualCost - b.annualCost;
-      if (this.sort === "price-desc") return b.annualCost - a.annualCost;
-      if (this.sort === "name-asc") return a.provider.localeCompare(b.provider);
-      if (this.sort === "name-desc") return b.provider.localeCompare(a.provider);
-      return 0;
-    });
-  }
-}
-
--------
-*/
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
@@ -66,7 +22,8 @@ export class ElectricityPageComponent {
     let filtered = this.tariffs.sort((a,b) => a.annualCost - b.annualCost);
     
     if (this.selectedType !== 'All') {
-      filtered = filtered.filter(t => t.type === this.selectedType);
+      var type = this.selectedType === "Basic" ? 1 : 2;
+      filtered = filtered.filter(t => t.type === type);
     }
 
     return this.sortOrder === 'asc' ? filtered : filtered.reverse();
